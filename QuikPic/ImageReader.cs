70b33c8cbf585884d75a86efcaf985e7f080004e
Avngarde <pivotguy888@gmail.com>
2025-09-06 10:59:47 +0200

    Add LoadImageFromPath

---
+++ QuikPic/ImageReader.cs
@@ -0,0 +1,15 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+
+namespace QuikPic
+{
+    public class ImageReader
+    {
+        public Image<Rgba32> LoadImageFromPath(string path)
+        {
+            Image<Rgba32> image = Image.Load<Rgba32>(path);
+
+            return image;
+        }
+    }
+}
