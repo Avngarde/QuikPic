618c40082f4cef296d0920bd2dd74f5ce4069eb3
Avngarde <pivotguy888@gmail.com>
2025-09-06 11:42:23 +0200

    Add LoadImageFromPath test

--- QuikPic.Tests/ImageReaderTests.cs
+++ QuikPic.Tests/ImageReaderTests.cs
@@ -1,11 +1,18 @@
+using SixLabors.ImageSharp.PixelFormats;
+
 namespace QuikPic.Tests
 {
     public class ImageReaderTests
     {
         [Fact]
-        public void Test1()
+        public void LoadImageFromPath_ForTestImagePath_ReturnsRgba32()
         {
+            string path = @"./TestImages/TestImage.png";
+            var file = ImageReader.LoadImageFromPath(path);
 
+            // TestImage.png is 272 x 193 pixels
+            Assert.Equal(272, file.Size.Width);
+            Assert.Equal(193, file.Size.Height);
         }
     }
 }
