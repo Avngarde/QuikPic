a354d02dc095317ecb11d9f48d4016a9c0d73304
Avngarde <pivotguy888@gmail.com>
2025-09-06 11:51:21 +0200

    Add LoadImageFromBytes test

--- QuikPic.Tests/ImageReaderTests.cs
+++ QuikPic.Tests/ImageReaderTests.cs
@@ -14,5 +14,17 @@ namespace QuikPic.Tests
             Assert.Equal(272, file.Size.Width);
             Assert.Equal(193, file.Size.Height);
         }
+
+        [Fact]
+        public void LoadImageFromBytes_ForTestImagePath_ReturnsRgba32()
+        {
+            string path = @"./TestImages/TestImage.png";
+            byte[] fileBytes = File.ReadAllBytes(path);
+            var file = ImageReader.LoadImageFromBytes(fileBytes);
+            
+            // TestImage.png is 272 x 193 pixels
+            Assert.Equal(272, file.Size.Width);
+            Assert.Equal(193, file.Size.Height);
+        }
     }
 }
