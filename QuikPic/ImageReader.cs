660f9d9ea7c8331e8219fe4ee46ccf01085cec72
Avngarde <pivotguy888@gmail.com>
2025-09-06 11:04:41 +0200

    Add LoadImageFromBytes

--- QuikPic/ImageReader.cs
+++ QuikPic/ImageReader.cs
@@ -8,6 +8,14 @@ namespace QuikPic
         public Image<Rgba32> LoadImageFromPath(string path)
         {
             Image<Rgba32> image = Image.Load<Rgba32>(path);
+            return image;
+        }
+
+        public Image<Rgba32> LoadImageFromBytes(byte[] imageData)
+        {
+            Stream fileStream = new MemoryStream(imageData);
+            Image<Rgba32> image = Image.Load<Rgba32>(fileStream);
+            fileStream.Close();
 
             return image;
         }
