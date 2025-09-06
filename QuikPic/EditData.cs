42efea47ad930a823e5430c2a5d663e1cdfa8c69
Avngarde <pivotguy888@gmail.com>
2025-09-06 12:20:02 +0200

    Add EditData class

---
+++ QuikPic/EditData.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace QuikPic;
+
+public class EditData
+{
+    public float Exposure { get; set; }
+    public float Brilliance { get; set; }
+    public float Shadows { get; set; }
+    public float Contrast { get; set; }
+    public float Brightness { get; set; }
+    public float Saturation{ get; set; }
+}
