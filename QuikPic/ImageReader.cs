f3fd960ffcc17f03165c62238333748f6935f218
Avngarde <pivotguy888@gmail.com>
2025-09-06 11:05:17 +0200

    Change ImageReader to static class

--- QuikPic/ImageReader.cs
+++ QuikPic/ImageReader.cs
@@ -3,15 +3,15 @@ using SixLabors.ImageSharp.PixelFormats;
 
 namespace QuikPic
 {
-    public class ImageReader
+    public static class ImageReader
     {
-        public Image<Rgba32> LoadImageFromPath(string path)
+        public static Image<Rgba32> LoadImageFromPath(string path)
         {
             Image<Rgba32> image = Image.Load<Rgba32>(path);
             return image;
         }
 
-        public Image<Rgba32> LoadImageFromBytes(byte[] imageData)
+        public static Image<Rgba32> LoadImageFromBytes(byte[] imageData)
         {
             Stream fileStream = new MemoryStream(imageData);
             Image<Rgba32> image = Image.Load<Rgba32>(fileStream);
