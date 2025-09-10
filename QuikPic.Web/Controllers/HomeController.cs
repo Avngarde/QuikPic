166ad9000a488ed5b37eb46b2205bed8fd41c51d
Avngarde <pivotguy888@gmail.com>
2025-09-10 20:28:37 +0200

    Add uploading file to edit

--- QuikPic.Web/Controllers/HomeController.cs
+++ QuikPic.Web/Controllers/HomeController.cs
@@ -18,6 +18,26 @@ public class HomeController : Controller
         return View();
     }
 
+    [HttpPost]
+    public IActionResult UploadFile(IFormFile file)
+    {
+        if (file != null && file.Length > 0)
+        {
+            var uploadDir = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/uploads");
+            if (!Directory.Exists(uploadDir))
+                Directory.CreateDirectory(uploadDir);
+
+            var filePath = Path.Combine(uploadDir, file.FileName);
+
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                file.CopyTo(stream);
+            }
+        }
+
+        return RedirectToAction("Index");
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
