779d888a6839a2c1277a1bad1c28a3c26cef9e3f
Avngarde <pivotguy888@gmail.com>
2025-09-27 22:55:07 +0200

    Add saving edited picture

--- QuikPic.Web/Controllers/EditController.cs
+++ QuikPic.Web/Controllers/EditController.cs
@@ -1,19 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Net.Mime;
+using Microsoft.AspNetCore.Mvc;
 
 namespace QuikPic.Web.Controllers
 {
     public class EditController : Controller
     {
+        private readonly IWebHostEnvironment _env;
+
+        public EditController(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+
         public IActionResult Index(string fileGuid)
         {
             ViewData["fileGuid"] = fileGuid;
-            
+
             return View();
         }
 
-        public IActionResult EditPhoto(EditData editData)
+        public IActionResult EditPhoto(EditData editData, string fileName)
         {
-            return View();
+            string fileNameTrimmed = fileName.TrimStart(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar);
+            var path = Path.Combine(_env.WebRootPath, fileNameTrimmed);
+
+            var image = ImageReader.LoadImageFromPath(path);
+            ImageProcessor imageProcessor = new(image);
+
+            imageProcessor.ProcessImage(editData);
+            var imageBytes = imageProcessor.GetImageBytes();
+
+            return File(imageBytes, MediaTypeNames.Image.Jpeg, "image.jpg");
         }
     }
 }
