058fee8882209eac5bff3c8323ea34160e390fa7
Avngarde <pivotguy888@gmail.com>
2025-10-03 20:18:59 +0200

    Add SignalR dependency

--- QuikPic.Web/Program.cs
+++ QuikPic.Web/Program.cs
@@ -1,7 +1,10 @@
+using Microsoft.AspNetCore.SignalR;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddSignalR();
 
 var app = builder.Build();
 
