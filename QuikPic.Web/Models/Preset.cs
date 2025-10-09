8780980d992ac2e4412e66f91856744cea8af5f1
Avngarde <pivotguy888@gmail.com>
2025-10-09 20:42:19 +0200

    Add Preset model

---
+++ QuikPic.Web/Models/Preset.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace QuikPic.Web.Models;
+
+public class Preset
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public float Brightness { get; set; }
+    public float Contrast { get; set; }
+    public float Saturation { get; set; }
+    public float Grayscale { get; set; }
+    public float Temperature { get; set; }
+    public float Tint { get; set; }
+    public float Grain { get; set; }
+    public float Vignette { get; set; }
+}
