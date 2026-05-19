b7005378596ff15aadcd953e8454634168a0f3c5
Avngarde <pivotguy888@gmail.com>
2026-05-19 11:40:29 +0200

    Add: Error type

---
+++ QuikPic.Web/Errors/Error.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace QuikPic.Web.Errors
+{
+    public enum ErrorType
+    {
+        FileNotFound,
+        FileIsNotAnImage,
+        AddPresetError,
+        EditPresetError,
+        DownloadError
+    }
+
+    public class Error
+    {
+        public string? ErrorMessage { get; set; }
+        public ErrorType ErrorType { get; set; }
+    }
+}
