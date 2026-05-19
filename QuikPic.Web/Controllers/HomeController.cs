610978cdeb7605228d4ccbeae761692b32ac3540
Avngarde <pivotguy888@gmail.com>
2026-05-19 11:04:14 +0200

    Add: Home view error message

--- QuikPic.Web/Controllers/HomeController.cs
+++ QuikPic.Web/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using QuikPic.Web.Models;
+using SixLabors.ImageSharp;
 
 namespace QuikPic.Web.Controllers;
 
@@ -15,39 +16,60 @@ public class HomeController : Controller
         _env = env;
     }
 
-    public IActionResult Index()
+    public IActionResult Index(string? errorMessage)
     {
+        ViewData["ErrorMessage"] = errorMessage;
         return View();
     }
 
     [HttpPost]
     public IActionResult UploadFile(IFormFile file)
     {
-        string filePath = "";
-        string fileGuid = "";
-        if (file != null && file.Length > 0) 
+        if (file == null || file.Length == 0)
+            return RedirectToAction("Index", "Home", new { errorMessage = "File not found" });
+
+        var uploadDir = Path.Combine(_env.WebRootPath, "uploads");
+
+        if (!Directory.Exists(uploadDir))
+            Directory.CreateDirectory(uploadDir);
+
+        var fileExtension = Path.GetExtension(file.FileName);
+        var fileGuid = Guid.NewGuid().ToString();
+        var fileName = fileGuid + fileExtension;
+        var filePath = Path.Combine(uploadDir, fileName);
+
+        using (var stream = file.OpenReadStream())
         {
-            var uploadDir = Path.Combine(_env.WebRootPath, "uploads");
-            if (!Directory.Exists(uploadDir))
-                Directory.CreateDirectory(uploadDir);
-
-            var uniqueFileGuid = Guid.NewGuid().ToString();
-            fileGuid = uniqueFileGuid + Path.GetExtension(file.FileName);
-
-            filePath = Path.Combine(uploadDir, uniqueFileGuid + Path.GetExtension(file.FileName));
-
-            using (var stream = new FileStream(filePath, FileMode.Create))
-            {
-                file.CopyTo(stream);
-            }
+            if (!CheckIfImage(stream))
+                return RedirectToAction("Index", "Home", new { errorMessage = "Selected file is not a valid image" });
         }
 
-        return RedirectToAction("Index", "Edit", new { fileGuid = fileGuid });
+        using (var fileStream = new FileStream(filePath, FileMode.Create))
+        {
+            file.CopyTo(fileStream);
+        }
+
+        return RedirectToAction("Index", "Edit", new { fileGuid = fileName });
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+    private bool CheckIfImage(Stream stream)
+    {
+        try
+        {
+            using (var img = Image.Load(stream))
+            {
+                return true;
+            }
+        }
+        catch (UnknownImageFormatException)
+        {
+            return false;
+        }
     }
 }
